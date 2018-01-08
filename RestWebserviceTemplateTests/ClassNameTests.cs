79b563c54fd1d19ea729bea0cd01b4d34703ba10
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2018-01-08 23:27:14 +0100

    Implementeret test på grænser i ClassName klassen

---
+++ RestWebserviceTemplateTests/ClassNameTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestWebserviceTemplate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestWebserviceTemplate.Tests
+{
+    [TestClass()]
+    public class ClassNameTests
+    {
+        [TestMethod()]
+        public void GetX4Correct()
+        {
+            //Arrange
+
+            var xx = new ClassName(99, "Navn", "string", 33, "string", 22);
+
+            //Act 
+
+            int xxActual = xx.GetX4();
+
+            //Assert
+
+            Assert.AreEqual(22,xxActual);
+        }
+
+        [TestMethod()]
+
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetX4TestForkertAntal()
+        {
+            //Arrange 
+
+            var test = new ClassName(100,"Navn","string",33,"string",66);
+
+            //Act
+
+            int x4 = test.GetX4();
+
+
+        }
+
+
+    }
+}
